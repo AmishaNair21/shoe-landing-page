d60cd0fa1d3f03ad9582b63fba07d404fa0a10d2
AmishaNair21 <amishamnair2002@gmail.com>
2024-03-21 20:12:04 +0530

    created landing page

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./App.css"
+import Navigation from './component/Navigation'
+import Hero from './component/Hero'
+
+function App() {
+  return (
+    <>
+  <Navigation/>
+   <Hero/>
+   
+   
+   
+   </>
+  
+  )
+}
+
+export default App
